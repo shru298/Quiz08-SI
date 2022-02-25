a2e5bcca36608d9d018922cbc228e6431b4576b0
shruti <shru.kharkar2916@gmail.com>
2022-02-25 01:15:30 -0500

    my-function commit

---
+++ my-function.js
@@ -0,0 +1,19 @@
+//Quiz 09 my-fucntion code 
+app.get('/say', function(req, res) {
+    //Define base url
+    let queryObject = url.parse(req.url, true).search;
+    let apiUrl = "";
+    if(queryObject!=null){
+        apiUrl='https://t9qcqaqs37.execute-api.us-east-1.amazonaws.com/test/say' + queryObject; }
+    else
+        apiUrl='https://t9qcqaqs37.execute-api.us-east-1.amazonaws.com/test/say';
+   //call api and return response
+   axios.get(apiUrl)
+ 
+   .then(response => {
+       console.log("test" +response.data);
+      res.json(response.data)
+    })
+    .catch(err => res.json({ error: err }))
+ })
+ 
